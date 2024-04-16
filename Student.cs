e71a6e9ba5dcd2820b36257f978a500f9a3dcbd6
Karl-Xavier <uwasbruno256@gmail.com>
2024-04-16 10:43:20 +0100

    Updated The File

---
+++ Student.cs
@@ -0,0 +1,14 @@
+class Student
+{
+    public string name;
+    public string major;
+    public double gpa;
+}
+
+public Student(string aName, string aMajor, double gpa)
+{
+    name = aName;
+    major = aMalor;
+    gpa = aGpa;
+}
+
